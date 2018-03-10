40ec9ad0333ac40d64fef269456bc71bcef2ca8b
HoangHuuDien <hoanghuudienk@gmail.com>
2018-03-10 23:17:11 +0700

    cha biet noi gi

--- Session03/Bai-Tap/jumbleword.py
+++ Session03/Bai-Tap/jumbleword.py
@@ -1,11 +1,26 @@
-from random import * #choice = bốc ngẫu nhiên 1 phần tử trong list ra
-word = list("champion")
+# from random import * #choice = bốc ngẫu nhiên 1 phần tử trong list ra
+# word = list("champion")
+#
+# while True:
+#     shuffle(word)
+#     print(*word, sep=' ')
+#     answer = input('Your answer: ')
+#     if answer == 'champion':
+#         print('Hura')
+#     else: print(':( Let\'s do it again')
+#     break
+#
+#3b
+from random import *
+word = ["Champion", "Mission", "Attack", "Kill"]
+
+x = choice(word)
 
 while True:
-    shuffle(word)
-    print(*word, sep=' ')
-    answer = input('Your answer: ')
-    if answer == 'champion':
+    shuffle(list('x'))
+    print(*x, sep=' ')
+    guess = input('Your answer : ')
+    if guess == x:
         print('Hura')
-    else: print(':( Let\'s do it again')
+    else: print('Not true')
     break
